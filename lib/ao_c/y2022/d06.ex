de71c3ff8d21c461048ba304589bf0dde14d3440
lennyeing <lennyeing@gmail.com>
2022-12-06 13:44:02 +0100

    mix format.

--- lib/ao_c/y2022/d06.ex
+++ lib/ao_c/y2022/d06.ex
@@ -9,19 +9,24 @@ defmodule AoC.Y2022.D06 do
     first_unique!(input, 14)
   end
 
-  @spec first_unique!(binary(), non_neg_integer(), binary(), non_neg_integer()) :: non_neg_integer()
-  def first_unique!(<<head::binary-size(1), rest::binary()>>, num \\ 4, buffer \\ "", current_pos \\ 0) do
+  @spec first_unique!(binary(), non_neg_integer(), binary(), non_neg_integer()) ::
+          non_neg_integer()
+  def first_unique!(
+        <<head::binary-size(1), rest::binary()>>,
+        num \\ 4,
+        buffer \\ "",
+        current_pos \\ 0
+      ) do
     buffer_len = num - 2
-    case buffer do
 
+    case buffer do
       <<buffer_head::binary-size(buffer_len), _last::binary-size(1)>> ->
         cond do
-
           String.graphemes(head <> buffer) == String.graphemes(head <> buffer) |> Enum.uniq() ->
             current_pos + 1
+
           true ->
             first_unique!(rest, num, head <> buffer_head, current_pos + 1)
-
         end
 
       _ ->
